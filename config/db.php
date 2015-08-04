e07868348b5980f266fb38f6ebb67b278605151c
xuhongxu <xuhongxu96@gmail.com>
2015-08-04 16:03:16 +0800

    clean

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=media',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
