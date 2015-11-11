8adb8ed5048e438b43a1747a7aa8bf7298d91521
xuhongxu <xuhongxu96@qq.com>
2015-11-11 17:51:33 +0800

    html5 for ie8

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -20,6 +20,12 @@ AppAsset::register($this);
 	<?= Html::csrfMetaTags() ?>
 	<title>北师大·融媒体 - <?= Html::encode($this->title) ?></title>
 	<?php $this->head() ?>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
 
